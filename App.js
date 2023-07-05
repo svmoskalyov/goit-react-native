4e6e400b87334e5b0fb3ce7e814c59ba666f9947
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-07-05 20:12:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from "@expo-google-fonts/roboto";
+import LoginScreen from "./Screens/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import BackgroundImage from "./assets/images/background.png";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoRegular: Roboto_400Regular,
+    RobotoMedium: Roboto_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>GoIT React Native</Text>
       <StatusBar style="auto" />
+      <ImageBackground
+        source={BackgroundImage}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <RegistrationScreen />
+        {/* <LoginScreen /> */}
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +37,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
   },
 });
